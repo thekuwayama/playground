a31b618e4a8b605a8fb19f10cff41b10f29c1b65
thekuwayama <thekuwayama@gmail.com>
2019-01-03 16:35:16 +0900

    Add ABC/086/A.cpp

---
+++ ABC/086/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+  int a, b;
+  cin >> a >> b;
+
+  if (a * b % 2) {
+    cout << "Odd" << endl;
+  } else {
+    cout << "Even" << endl;
+  }
+  return EXIT_SUCCESS;
+}
