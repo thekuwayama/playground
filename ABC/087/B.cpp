17921f98998343970e8e465e24f057b9d2704c39
thekuwayama <thekuwayama@gmail.com>
2019-01-04 21:37:19 +0900

    Add ABC/087/B.cpp

---
+++ ABC/087/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+  int a, b, c, x;
+  cin >> a >> b >> c >> x;
+
+  int res = 0;
+  for (int i = 0; i <= a; i++) {
+    for (int j = 0; j <= b; j++) {
+      for (int k = 0; k <= c; k++) {
+        if (500 * i + 100 * j + 50 * k == x) {
+          res++;
+        }
+      }
+    }
+  }
+  cout << res << endl;
+  return EXIT_SUCCESS;
+}
