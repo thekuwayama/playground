5fed752ab5f08d748ce9fe8281576d1a39bf686f
thekuwayama <thekuwayama@gmail.com>
2019-01-27 07:01:45 +0900

    Add Content-Security-Policy-Report-Only header

--- rails/sample_app/config/initializers/secure_headers.rb
+++ rails/sample_app/config/initializers/secure_headers.rb
@@ -1,4 +1,13 @@
 SecureHeaders::Configuration.default do |config|
+  gravatar = "secure.gravatar.com"
+  subdomain = ENV['REPORT_URI_SUBDOMAIN']
+  report_uri = "https://#{subdomain}.report-uri.com/r/d/csp/reportOnly"
+  config.csp_report_only = {
+    default_src: %w('self'),
+    img_src: ["'self'", gravatar],
+    script_src: %w('self'),
+    report_uri: [report_uri],
+  }
   config.csp = SecureHeaders::OPT_OUT
   config.hsts = SecureHeaders::OPT_OUT
   config.x_frame_options = SecureHeaders::OPT_OUT
