fe4f4b5e6009e0215cc29efea482a6d34ab565c0
thekuwayama <thekuwayama@gmail.com>
2019-03-18 19:05:20 +0900

    generate key using prime256v1

--- ruby/sample_prime256v1.rb
+++ ruby/sample_prime256v1.rb
@@ -1,5 +1,6 @@
 require 'openssl'
 
+# testcase
 client_private_key = <<BIN.split.map(&:hex).map(&:chr).join
   ab 54 73 46 7e 19 34 6c     eb 0a 04 14 e4 1d a2 1d
   4d 24 45 bc 30 25 af e9     7c 4e 8d c8 d5 13 da 39
@@ -27,3 +28,27 @@ puts 'ok' if shared_secret == <<BIN.split.map(&:hex).map(&:chr).join
   c1 42 ce 13 ca 11 b5 c2     23 36 52 e6 3a d3 d9 78
   44 f1 62 1f bf b9 de 69     d5 47 dc 8f ed ea be b4
 BIN
+
+# generate key
+client_ec = OpenSSL::PKey::EC.new('prime256v1')
+client_ec.generate_key!
+client_key_exchange = client_ec.public_key.to_octet_string(:uncompressed)
+
+server_ec = OpenSSL::PKey::EC.new('prime256v1')
+server_ec.generate_key!
+server_key_exchange = server_ec.public_key.to_octet_string(:uncompressed)
+
+client_shared_key = client_ec.dh_compute_key(
+  OpenSSL::PKey::EC::Point.new(
+    OpenSSL::PKey::EC::Group.new('prime256v1'),
+    server_key_exchange
+  )
+)
+server_shared_key = server_ec.dh_compute_key(
+  OpenSSL::PKey::EC::Point.new(
+    OpenSSL::PKey::EC::Group.new('prime256v1'),
+    client_key_exchange
+  )
+)
+
+puts 'ok' if client_shared_key == server_shared_key
