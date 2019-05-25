b29c39b59c9908313302ce99f6ad447a46a4081f
thekuwayama <thekuwayama@gmail.com>
2019-05-25 09:58:25 +0900

    update gen_certs.rb; gen rsassaPss_sha{256,384}_server.{crt,key}

--- ruby/gen_certs.rb
+++ ruby/gen_certs.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'openssl'
 
 TMP_DIR = __dir__ + '/tmp'
 CA_CRT = TMP_DIR + '/rsa_ca.crt'
@@ -18,21 +19,23 @@ subjectAltName=DNS:localhost
 BIN
 File.write(EXTFILE, extfile)
 
-`openssl genrsa 4096 > #{CA_KEY}`
-`openssl req -new -x509 -days 3650 -sha256 -key #{CA_KEY} -subj "/CN=test-ca" > #{CA_CRT}`
+# `openssl genrsa 2048 > #{CA_KEY}`
+# `openssl req -new -x509 -days 3650 -sha256 -key #{CA_KEY} -subj "/CN=test-ca" > #{CA_CRT}`
 
-['prime256v1', 'secp384r1', 'secp521r1'].each_with_index do |curve, i|
+['prime256v1', 'secp384r1', 'secp521r1'].each do |curve|
   server_key = TMP_DIR + "/#{curve}_server.key"
   server_crt = TMP_DIR + "/#{curve}_server.crt"
   `openssl ecparam -name #{curve} -genkey -noout > #{server_key}`
   `openssl req -new -sha256 -key #{server_key} -subj "/CN=localhost" > #{CSR}`
-  `openssl x509 -req -in #{CSR} -days 3650 -CA #{CA_CRT} -CAkey #{CA_KEY} -set_serial #{101 + i} -extensions v3_ext -extfile #{EXTFILE} > #{server_crt}`
+  `openssl x509 -req -in #{CSR} -days 3650 -CA #{CA_CRT} -CAkey #{CA_KEY} -extensions v3_ext -extfile #{EXTFILE} -set_serial #{OpenSSL::BN.rand(64).to_i} > #{server_crt}`
 end
 
-server_key = TMP_DIR + "/rsassaPss_server.key"
-server_crt = TMP_DIR + "/rsassaPss_server.crt"
-`openssl genrsa 4096 > #{server_key}`
-`openssl req -new -sha256 -key #{server_key} -subj "/CN=localhost" > #{CSR}`
-`openssl x509 -req -in #{CSR} -days 3650 -CA #{CA_CRT} -CAkey #{CA_KEY} -set_serial 201 -extensions v3_ext -extfile #{EXTFILE} -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:32 > #{server_crt}`
+[256, 384, 512].each do |hs|
+  server_key = TMP_DIR + "/rsassaPss_sha#{hs}_server.key"
+  server_crt = TMP_DIR + "/rsassaPss_sha#{hs}_server.crt"
+  `openssl genrsa 2048 > #{server_key}`
+  `openssl req -new -sha#{hs} -key #{server_key} -subj "/CN=localhost" > #{CSR}`
+  `openssl x509 -req -in #{CSR} -days 3650 -sha#{hs} -CA #{CA_CRT} -CAkey #{CA_KEY} -extensions v3_ext -extfile #{EXTFILE} -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:#{hs / 8} -set_serial #{OpenSSL::BN.rand(64).to_i} > #{server_crt}`
+end
 
-# openssl x509 -in tmp/#{CRT} -noout -text
+# openssl x509 -in tmp/$CRT -noout -text
