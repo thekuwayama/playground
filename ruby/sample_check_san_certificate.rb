8b1ea11d6036839cb96c23e6abbfd9012e6f937f
thekuwayama <thekuwayama@gmail.com>
2019-11-12 21:30:05 +0900

    update sample_check_san_certificate.rb

--- ruby/sample_check_san_certificate.rb
+++ ruby/sample_check_san_certificate.rb
@@ -1,19 +1,25 @@
+#!/usr/bin/env ruby
+
 require 'openssl'
-require 'open-uri'
 
-raise 'pass server_name' if ARGV.length == 0
+# get certificate from TLS Server(HOSTNAME:PORT)
+hostname, port = (ARGV[0] || 'localhost:4433').split(':')
+socket = TCPSocket.new(hostname, port)
+ssl = OpenSSL::SSL::SSLSocket.new(socket)
+ssl.connect
+server_cert = ssl.peer_cert
+ssl.close
+socket.close
 
-# using OpenSSL::X509::DEFAULT_CERT_FILE
-store = OpenSSL::X509::Store.new
-store.set_default_paths
+# parse certificate
+san = server_cert.extensions.find { |ex| ex.oid == 'subjectAltName' }
+raise 'NO SAN' if san.nil?
 
-# get certificate from stdin
-cert = OpenSSL::X509::Certificate.new(STDIN.read)
-
-san = cert.extensions.find { |ex| ex.oid == 'subjectAltName' }
 ostr = OpenSSL::ASN1.decode(san.to_der).value.last
-p OpenSSL::ASN1.decode(ostr.value).map(&:value)
-               .map { |uri| uri.gsub('.', '\.').gsub('*', '.*') }
-               .any? { |uri|  ARGV[0].match(/#{uri}/) } unless san.nil?
+pp uris = OpenSSL::ASN1.decode(ostr.value).map(&:value)
 
-# echo | openssl s_client -connect localhost:443 -showcerts 2>/dev/null | openssl x509 -outform PEM | ruby sample_check_san_certificate.rb localhost
+# HOSTNAME is included in SANs?
+puts uris.map { |uri| uri.gsub('.', '\.').gsub('*', '.*') }
+         .any? { |uri|  ARGV[0].match(/#{uri}/) }
+
+# ruby sample_check_san_certificate.rb localhost:4433
