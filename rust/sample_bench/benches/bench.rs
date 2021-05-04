9a815f61c0904ea71c394135255fe366c61f3e52
thekuwayama <thekuwayama@gmail.com>
2021-05-04 19:45:24 +0900

    add rust/sample_bench

---
+++ rust/sample_bench/benches/bench.rs
@@ -0,0 +1,8 @@
+#![feature(test)]
+
+extern crate test;
+
+#[bench]
+fn sum_bench(b: &mut test::Bencher) {
+    b.iter(|| sample_bench::add(1, 2))
+}
---
+++ rust/sample_bench/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add(x: u8, y: u8) -> u8 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
