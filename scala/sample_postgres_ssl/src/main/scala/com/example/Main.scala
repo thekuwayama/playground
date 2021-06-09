59f9604cc8afe6a7062ff6a4997986cd4f78198e
thekuwayama <thekuwayama@gmail.com>
2021-06-10 02:23:23 +0900

    using intermediate certs

--- scala/sample_postgres_ssl/src/main/scala/com/example/Main.scala
+++ scala/sample_postgres_ssl/src/main/scala/com/example/Main.scala
@@ -5,10 +5,10 @@ import java.sql.DriverManager
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val host: String             = "localhost"
-    val port: Int                = 5432
-    val database: String         = "postgres"
-    val user: String             = "postgres"
+    val host: String             = sys.env.getOrElse("HOST", "localhost")
+    val port: Int                = sys.env.getOrElse("PORT", "5432").toInt
+    val database: String         = sys.env.getOrElse("DBNAME", "postgres")
+    val user: String             = sys.env.getOrElse("USER", "postgres")
     val password: Option[String] = None
 
     val url = genJdbcUrl(host, port, database, user)
@@ -25,13 +25,13 @@ object Main {
     val currentDir = new File(".").getAbsoluteFile.getParent
 
     s"jdbc:postgresql://$host:$port/$database" + (Map(
-      "user" -> user,
-      "ssl" -> true,
+      "user"                     -> user,
+      "ssl"                      -> true,
       "ssl_max_protocol_version" -> "TLSv1.2",
-      "clientcert" -> "verify-full",
-      "sslcert" -> s"$currentDir/client.crt",
-      "sslkey" -> s"$currentDir/client.key.p8",
-      "sslrootcert" -> s"$currentDir/root.crt"
+      "clientcert"               -> "verify-full",
+      "sslcert"                  -> s"$currentDir/client.crt",
+      "sslkey"                   -> s"$currentDir/client.key.p8",
+      "sslrootcert"              -> s"$currentDir/server-root.crt",
     )).map { case (k, v) => s"$k=$v" }.mkString("?", "&", "")
   }
 }
