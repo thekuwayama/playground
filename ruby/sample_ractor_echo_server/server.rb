34475368ea8e4734db4976a6759501bab0b19ee4
thekuwayama <thekuwayama@gmail.com>
2025-11-18 23:44:37 +0900

    improvement: Ractor Port

--- ruby/sample_ractor_echo_server/server.rb
+++ ruby/sample_ractor_echo_server/server.rb
@@ -1,35 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'etc'
 require 'socket'
 
 port = ARGV[0] || 8888
 
-queue = Ractor.new do
-  loop do
-    Ractor.yield(Ractor.recv, move: true)
-  end
-end
+Socket.tcp_server_loop(port) do |socket, addr|
+  p addr
 
-Etc.nprocessors.times do
-  Ractor.new(queue) do |q|
+  writer = Ractor.new do
     loop do
-      s = q.take
       begin
-        while (buf = s.gets)
-          s.write buf
+        while (sock_buf = Ractor.receive)
+          break if sock_buf.nil?
+
+          sock, buf = sock_buf
+          sock.write(buf)
         end
       rescue StandardError => e
         warn e
       ensure
-        s&.close
+        sock&.close
       end
     end
   end
-end
 
-Socket.tcp_server_loop(port) do |socket, addr|
-  p addr
-  queue.send(socket, move: true)
+  loop do
+    buf = socket.gets
+    writer << [socket, buf]
+  end
 end
