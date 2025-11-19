63f1ba1949dc24e1da98ed67c1a9af10ec55ce44
thekuwayama <thekuwayama@gmail.com>
2025-11-20 01:32:26 +0900

    Writer

--- ruby/sample_ractor_echo_server/server.rb
+++ ruby/sample_ractor_echo_server/server.rb
@@ -5,28 +5,30 @@ require 'socket'
 
 port = ARGV[0] || 8888
 
-Socket.tcp_server_loop(port) do |socket, addr|
-  p addr
+class Writer
+  attr_reader :port
 
-  writer = Ractor.new do
-    loop do
-      begin
-        while (sock_buf = Ractor.receive)
-          break if sock_buf.nil?
+  def initialize
+    @port = Ractor.new do
+      loop do
+        begin
+          sock, s = Ractor.receive
 
-          sock, buf = sock_buf
-          sock.write(buf)
+          sock.write(s)
+        rescue StandardError => e
+          warn e
         end
-      rescue StandardError => e
-        warn e
-      ensure
-        sock&.close
       end
     end
   end
+end
 
+Socket.tcp_server_loop(port) do |socket, addr|
+  p addr
+
+  writer = Writer.new
   loop do
     buf = socket.gets
-    writer << [socket, buf]
+    writer.port << [socket, buf]
   end
 end
